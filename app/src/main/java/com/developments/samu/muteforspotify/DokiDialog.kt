374b0916cb7130b088bb4a8da6dea3f0b5f0421e
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-07-01 14:31:27 +0200

    Remove unused doki dialog

--- app/src/main/java/com/developments/samu/muteforspotify/DokiDialog.kt
+++ app/src/main/java/com/developments/samu/muteforspotify/DokiDialog.kt
@@ -1,47 +1,14 @@
 package com.developments.samu.muteforspotify
 
-import android.app.Dialog
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dev.doubledot.doki.views.DokiContentView
 
-
-class DokiDialog : DialogFragment() {
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dokiCustomView = View.inflate(context, R.layout.activity_doki, null)
-        dokiCustomView?.findViewById<DokiContentView?>(R.id.doki_content)?.let {
-            it.setButtonsVisibility(false)
-            it.loadContent()
-        }
-        return MaterialAlertDialogBuilder(requireContext()).apply {
-            setView(dokiCustomView)
-            setPositiveButton(R.string.doki_close) { _, _ ->
-                dismiss()
-            }
-        }.create()
-    }
-
-    fun show(context: FragmentActivity, tag: String = DOKI_DIALOG_TAG) {
-        show(context.supportFragmentManager, tag)
-    }
-
-    companion object {
-        private const val DOKI_DIALOG_TAG = "doki_dialog"
-        fun show(context: FragmentActivity, tag: String = DOKI_DIALOG_TAG) {
-            DokiDialog().show(context, tag)
-        }
-    }
-}
 
 class DokiThemedActivity : AppCompatActivity() {
 
     private val dokiContent: DokiContentView? by lazy {
-        findViewById<DokiContentView?>(R.id.doki_content)
+        findViewById(R.id.doki_content)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,5 +18,4 @@ class DokiThemedActivity : AppCompatActivity() {
         dokiContent?.setOnCloseListener { supportFinishAfterTransition() }
         dokiContent?.loadContent()
     }
-
 }
